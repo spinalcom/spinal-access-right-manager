c0c0b81c3649bd34d80bc4619bf6a724b3e8577a
LorisC <loris@ciprin.fr>
2019-03-15 15:54:04 +0100

    fix right and create default methode

--- src/accesRigthManagerButton.ts
+++ src/accesRigthManagerButton.ts
@@ -1,12 +1,16 @@
-import { UserProfileLst } from "./Models/UserProfileLst";
+import { RoleLst } from "./Models/RoleLst";
+import { Role } from "./Models/Role";
 import { UserProfile } from "./Models/UserProfile";
 import { AppProfileLst } from "./Models/AppProfileLst";
 import { AppProfile } from "./Models/AppProfile";
 
 import * as spinalEnvDriveCore from "spinal-env-drive-core";
+import { Directory } from "spinal-core-connectorjs_type";
 
 const CONFIG_PATH = "/etc/config/";
-const USER_PROFILE_LST_PATH = CONFIG_PATH + "UserProfileLst";
+const USERS_FILE_PATH = "/etc/users";
+const USERS_PROFILE_DIR_NAME = "UserProfileDir";
+const ROLE_LST_PATH = CONFIG_PATH + "UserProfileLst";
 const APP_PROFILE_LST_PATH = CONFIG_PATH + "AppProfileLst";
 const ADMIN_NAME = 'Admin';
 const ADMIN_DESCRIPTION = 'Administrator of the spinal platform';
@@ -47,48 +51,48 @@ export class SpinalAdminInit extends spinalEnvDriveCore.SpinalDrive_App {
     authService
       .wait_connect()
       .then(() => {
-        SpinalAdminInit.initUserProfileLst(ngSpinalCore)
-          .then(SpinalAdminInit.initAppProfileLst.bind(this,ngSpinalCore))
-
+        SpinalAdminInit.initRoleLst(ngSpinalCore)
+          .then(SpinalAdminInit.initAppProfileLst.bind(this, ngSpinalCore))
+          .then(SpinalAdminInit.initDefaultUser.bind(this, ngSpinalCore));
       })
   };
 
-  static initUserProfileLst(ngSpinalCore) : Promise<void> {
-    return ngSpinalCore.load(USER_PROFILE_LST_PATH)
-      .then(SpinalAdminInit.onUserProfileLstLoadSuccessful)
-      .catch(SpinalAdminInit.onUserProfileLstLoadFail.bind(this,ngSpinalCore));
+  static initRoleLst(ngSpinalCore): Promise<void> {
+    return ngSpinalCore.load(ROLE_LST_PATH)
+      .then(SpinalAdminInit.onRoleLstLoadSuccessful)
+      .catch(SpinalAdminInit.onRoleLstLoadFail.bind(this, ngSpinalCore));
   }
 
-  static initAppProfileLst(ngSpinalCore) : Promise<void> {
+  static initAppProfileLst(ngSpinalCore): Promise<void> {
     return ngSpinalCore.load(APP_PROFILE_LST_PATH)
       .then(SpinalAdminInit.onAppProfileLstLoadSuccessful)
-      .catch(SpinalAdminInit.onAppProfileLstLoadFail.bind(this,ngSpinalCore));
+      .catch(SpinalAdminInit.onAppProfileLstLoadFail.bind(this, ngSpinalCore));
   }
 
-  static onUserProfileLstLoadSuccessful() : Promise<void>{
+  static onRoleLstLoadSuccessful(): Promise<void> {
     return Promise.resolve();
   }
 
-  static onUserProfileLstLoadFail(ngSpinalCore) : Promise<void> {
-    const userProfileLst: UserProfileLst = new UserProfileLst();
+  static onRoleLstLoadFail(ngSpinalCore): Promise<void> {
+    const userProfileLst: RoleLst = new RoleLst();
     for (let i = 0; i < USERS.length; i++) {
       const user = USERS[i];
       //start id = 1
-      userProfileLst.users.push(new UserProfile(i + 1, user.name, user.description));
+      userProfileLst.users.push(new Role(i + 1, user.name, user.description));
     }
-    return ngSpinalCore.store(userProfileLst, USER_PROFILE_LST_PATH)
-      .then(SpinalAdminInit.onUserProfileLstLoadSuccessful)
+    return ngSpinalCore.store(userProfileLst, ROLE_LST_PATH)
+      .then(SpinalAdminInit.onRoleLstLoadSuccessful)
       .catch(() => {
         console.log('store rejection from onUserProfileLstLoadFail');
         return Promise.resolve();
       })
   }
 
-  static onAppProfileLstLoadSuccessful() : Promise<void>{
+  static onAppProfileLstLoadSuccessful(): Promise<void> {
     return Promise.resolve();
   }
 
-  static onAppProfileLstLoadFail(ngSpinalCore) : Promise<void> {
+  static onAppProfileLstLoadFail(ngSpinalCore): Promise<void> {
 
     const appProfileLst: AppProfileLst = new AppProfileLst();
     for (let i = 0; i < 32; i++) {
@@ -105,4 +109,39 @@ export class SpinalAdminInit extends spinalEnvDriveCore.SpinalDrive_App {
         return Promise.resolve();
       })
   }
+
+  static getUserProfileDir(ngSpinalCore) {
+    return ngSpinalCore.load('/etc')
+      .then(etcDir => {
+        for (let i = 0; i < etcDir.length; i++) {
+          const file = etcDir[i];
+          if (file.name === USERS_PROFILE_DIR_NAME)
+            return file;
+        }
+        const Dir = new Directory();
+        etcDir.add_file(USERS_PROFILE_DIR_NAME, Dir);
+        return Dir;
+      })
+  }
+
+  static initDefaultUser(ngSpinalCore) {
+    return SpinalAdminInit.getUserProfileDir(ngSpinalCore)
+      .then((dir) => {
+        return ngSpinalCore.load(USERS_FILE_PATH)
+          .then((users) => {
+            const promises = [];
+            for (let i = 0; i < users.length; i++) {
+              const user = users[i];
+              if (!dir.has(user.name)) {
+                //6 is the id of default user role. 1 is admin role
+                const type = (user.type == 0) ? 1 : 5;
+                const userProfile: UserProfile = new UserProfile([type]);
+                dir.add_file(user.name, userProfile);
+              }
+            }
+          })
+      });
+  }
+
+
 }
