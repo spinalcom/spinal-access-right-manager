5deeb1baa7dfb9ebc65daca26c7592201993086a
LorisC <loris@ciprin.fr>
2019-03-15 15:54:58 +0100

    create model for access right

--- declarations/accesRigthManagerButton.d.ts
+++ declarations/accesRigthManagerButton.d.ts
@@ -1,13 +1,14 @@
-declare const spinalEnvDriveCore: any;
+import * as spinalEnvDriveCore from "spinal-env-drive-core";
 export declare class SpinalAdminInit extends spinalEnvDriveCore.SpinalDrive_App {
     constructor();
     action(obj: any): void;
     init(authService: any, ngSpinalCore: any): void;
-    static initUserProfileLst(ngSpinalCore: any): Promise<void>;
+    static initRoleLst(ngSpinalCore: any): Promise<void>;
     static initAppProfileLst(ngSpinalCore: any): Promise<void>;
-    static onUserProfileLstLoadSuccessful(userProfileLst: any): Promise<void>;
-    static onUserProfileLstLoadFail(ngSpinalCore: any): Promise<void>;
+    static onRoleLstLoadSuccessful(): Promise<void>;
+    static onRoleLstLoadFail(ngSpinalCore: any): Promise<void>;
     static onAppProfileLstLoadSuccessful(): Promise<void>;
     static onAppProfileLstLoadFail(ngSpinalCore: any): Promise<void>;
+    static getUserProfileDir(ngSpinalCore: any): any;
+    static initDefaultUser(ngSpinalCore: any): any;
 }
-export {};
