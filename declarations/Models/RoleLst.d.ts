f9b3db9a7a06d55c3b84ff67b611b425564e401c
LorisC <loris@ciprin.fr>
2019-03-15 15:54:49 +0100

    create model for access right

--- declarations/Models/RoleLst.d.ts
+++ declarations/Models/RoleLst.d.ts
@@ -1,6 +1,6 @@
 import { Model } from 'spinal-core-connectorjs_type';
 import { Role } from "./UserProfile";
-export declare class UserProfileLst extends Model {
+export declare class RoleLst extends Model {
     users: spinal.Lst<Role>;
     constructor();
 }
--- declarations/Models/UserProfile.d.ts
+++ declarations/Models/UserProfile.d.ts
@@ -1,7 +1,5 @@
 import { Model } from 'spinal-core-connectorjs_type';
 export declare class UserProfile extends Model {
-    id: spinal.Str;
-    name: spinal.Str;
-    description: spinal.Str;
-    constructor(id: number, name: string, description: string);
+    appProfiles: spinal.Lst<spinal.Val>;
+    constructor(appProfiles?: number[]);
 }
