a07cf3f9db9dd1cdc2ecdcaae275d6dd90806a68
LorisC <loris@ciprin.fr>
2019-03-18 16:39:46 +0100

    automatize the initialization

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require('spinal-env-drive-core');
 const accesRigthManagerButton_1 = require("./accesRigthManagerButton");
-const authService = window.angular.element('body > div').injector().get('authService');
-const ngSpinalCore = window.angular.element('body > div').injector().get('ngSpinalCore');
+const angular = require('angular');
+const authService = angular.element('body > div').injector().get('authService');
+const ngSpinalCore = angular.element('body > div').injector().get('ngSpinalCore');
 accesRigthManagerButton_1.SpinalAdminInit.init(authService, ngSpinalCore);
 //# sourceMappingURL=index.js.map
